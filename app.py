01f3016eaad2123a861839162924f848359d54f1
Shraddha Agarwal <shraddha19@iiserb.ac.in>
2023-12-11 03:08:22 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,91 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder
+)
+import streamlit as st
+from streamlit_chat import message
+from utils import *
+import os
+
+
+import os
+
+PINECONE_API_KEY = os.environ["PINECONE_API_KEY"]
+HF_API_KEY = os.environ["HF_API_KEY"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+
+
+st.subheader("Reseacher Assistant GPT for Identification of Nutraceuticals")
+markdown_content = """
+**Developed By:** Shraddha Agarwal (19294)  
+**Supervisor:** Dr. Tanmay Basu  
+**Organization:** Indian Institute of Science Education and Research Bhopal
+"""
+
+# Use st.markdown to display the content
+st.markdown(markdown_content)
+
+available_models = ["gpt-3.5-turbo", "text-davinci-003", "gpt-3.5-large"]  # Add other models as needed
+selected_model = st.selectbox("Select the model to use:", available_models)
+
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ["How can I assist you?"]
+
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key= OPENAI_API_KEY)
+
+if 'buffer_memory' not in st.session_state:
+            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+
+
+system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully as possible using the provided context, 
+and if the answer is not contained within the text below, say 'I don't know'""")
+
+
+human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
+
+prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
+
+conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
+
+
+
+
+# container for chat history
+response_container = st.container()
+# container for text box
+textcontainer = st.container()
+
+
+with textcontainer:
+    query = st.text_input("Query: ", key="input")
+    if query:
+        with st.spinner("typing..."):
+            conversation_string = get_conversation_string()
+            # st.code(conversation_string)
+            refined_query = query_refiner(conversation_string, query)
+            st.subheader("Refined Query:")
+            st.write(refined_query)
+            context = find_match(refined_query)
+            # print(context)  
+            response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
+        st.session_state.requests.append(query)
+        st.session_state.responses.append(response) 
+with response_container:
+    if st.session_state['responses']:
+
+        for i in range(len(st.session_state['responses'])):
+            message(st.session_state['responses'][i],key=str(i))
+            if i < len(st.session_state['requests']):
+                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+
+
+          
---
+++ utils.py
@@ -0,0 +1,61 @@
+from sentence_transformers import SentenceTransformer
+import pinecone
+import openai
+import streamlit as st
+import os
+
+PINECONE_API_KEY = os.environ["PINECONE_API_KEY"]
+HF_API_KEY = os.environ["HF_API_KEY"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+openai.api_key = OPENAI_API_KEY
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+pinecone.init(api_key=PINECONE_API_KEY, environment='gcp-starter')
+index = pinecone.Index('nutraceuticlas-chatbot')
+
+def find_match(input):
+    input_em = model.encode(input).tolist()
+    result = index.query(input_em, top_k=2, includeMetadata=True)
+    return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
+
+# def query_refiner(conversation, query):
+
+#     response = openai.Completion.create(
+#     model="gpt-3.5-turbo",
+#     prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+#     temperature=0.7,
+#     max_tokens=256,
+#     top_p=1,
+#     frequency_penalty=0,
+#     presence_penalty=0
+#     )
+#     return response['choices'][0]['text']
+
+
+def query_refiner(conversation, query):
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",  # or the specific chat model you are using
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": f"CONVERSATION LOG:\n{conversation}\n\nQuery: {query}"}
+            ]
+        )
+        return response['choices'][0]['message']['content']
+    except Exception as e:
+        print(f"Error in query_refiner: {e}")
+        return query
+
+
+
+
+
+
+def get_conversation_string():
+    conversation_string = ""
+    for i in range(len(st.session_state['responses'])-1):
+        
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    return conversation_string
